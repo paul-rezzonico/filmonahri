debb1864b606f1e3299e325518e6d2c4af792f5b
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:33 -0400

    feat(service) add user service

--- server/manifest.js
+++ server/manifest.js
@@ -12,14 +12,14 @@ module.exports = new Confidence.Store({
     server: {
         host: 'localhost',
         port: {
-            $param: 'PORT',
+            $env: 'PORT',
             $coerce: 'number',
             $default: 3000
         },
         debug: {
-            $filter: 'NODE_ENV',
+            $filter: { $env: 'NODE_ENV' },
             $default: {
-                log: ['error', 'start'],
+                log: ['error'],
                 request: ['error']
             },
             production: {
@@ -37,8 +37,30 @@ module.exports = new Confidence.Store({
                 plugin: './plugins/swagger'
             },
             {
+                plugin  : '@hapipal/schwifty',
+                options : {
+                    $filter    : 'NODE_ENV',
+                    $default   : {},
+                    $base      : {
+                        migrateOnStart : true,
+                        knex           : {
+                            client     : 'mysql',
+                            connection : {
+                                host     : process.env.DB_HOST || '0.0.0.0',
+                                user     : process.env.DB_USER || 'root',
+                                password : process.env.DB_PASSWORD || 'hapi',
+                                database : process.env.DB_DATABASE || 'user'
+                            }
+                        }
+                },
+                production : {
+                        migrateOnStart : false
+                    }
+                }
+            },
+            {
                 plugin: {
-                    $filter: 'NODE_ENV',
+                    $filter: { $env: 'NODE_ENV' },
                     $default: '@hapipal/hpal-debug',
                     production: Toys.noop
                 }
