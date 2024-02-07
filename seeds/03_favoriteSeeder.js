e2b30aef0d41c086b3dfd75f3cf9377081ab1bf7
Paul Rezzonico <paul.rezzonico@desktop>
2024-02-07 13:25:59 +0100

    Fix(seeders) refacto favorite_seeder

--- seeds/03_favoriteSeeder.js
+++ seeds/03_favoriteSeeder.js
@@ -4,15 +4,15 @@ exports.seed = function (knex) {
         .then(function () {
             // Inserts seed entries
             return knex('user_favorites').insert([
-                {userId: 1, filmId: 6},
-                {userId: 2, filmId: 5},
-                {userId: 3, filmId: 1},
-                {userId: 4, filmId: 4},
-                {userId: 5, filmId: 5},
-                {userId: 6, filmId: 2},
-                {userId: 5, filmId: 6},
-                {userId: 1, filmId: 5},
-                {userId: 2, filmId: 4},
+                {user_id: 1, film_id: 6},
+                {user_id: 2, film_id: 5},
+                {user_id: 3, film_id: 1},
+                {user_id: 4, film_id: 4},
+                {user_id: 5, film_id: 5},
+                {user_id: 6, film_id: 2},
+                {user_id: 5, film_id: 6},
+                {user_id: 1, film_id: 5},
+                {user_id: 2, film_id: 4},
             ]);
         });
 };
